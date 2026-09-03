a233e83f525faf544128c3872381c234f97ed392
brunopolii <brunopoli95@gmail.com>
2026-09-03 18:32:37 -0300

    style: improve card hierarchy and spacing

--- lib/widgets/app_card.dart
+++ lib/widgets/app_card.dart
@@ -1,20 +1,2 @@
-
 import 'package:flutter/material.dart';
-
-class AppCard extends StatelessWidget {
-  final Widget child;
-
-  const AppCard({super.key, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      margin: const EdgeInsets.only(bottom: 12),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: child,
-      ),
-    );
-  }
-}
+class AppCard extends StatelessWidget{final Widget child;const AppCard({super.key,required this.child});@override Widget build(BuildContext context){return Card(margin:const EdgeInsets.only(bottom:12),elevation:1,shadowColor:Theme.of(context).colorScheme.shadow.withOpacity(.10),shape:RoundedRectangleBorder(borderRadius:BorderRadius.circular(20)),clipBehavior:Clip.antiAlias,child:Padding(padding:const EdgeInsets.all(17),child:child));}}
