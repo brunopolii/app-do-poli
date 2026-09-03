7e641bb5e8598e2e011c0e306528c17c1fe2e491
brunopolii <brunopoli95@gmail.com>
2026-09-03 19:03:54 -0300

    fix: clean app card widget

--- lib/widgets/app_card.dart
+++ lib/widgets/app_card.dart
@@ -1,2 +1,18 @@
 import 'package:flutter/material.dart';
-class AppCard extends StatelessWidget{final Widget child;const AppCard({super.key,required this.child});@override Widget build(BuildContext context){return Card(margin:const EdgeInsets.only(bottom:12),elevation:1,shadowColor:Theme.of(context).colorScheme.shadow.withOpacity(.10),shape:RoundedRectangleBorder(borderRadius:BorderRadius.circular(20)),clipBehavior:Clip.antiAlias,child:Padding(padding:const EdgeInsets.all(17),child:child));}}
+
+class AppCard extends StatelessWidget {
+  final Widget child;
+  const AppCard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12),
+      elevation: 1,
+      shadowColor: Theme.of(context).colorScheme.shadow.withValues(alpha: .10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      clipBehavior: Clip.antiAlias,
+      child: Padding(padding: const EdgeInsets.all(17), child: child),
+    );
+  }
+}
