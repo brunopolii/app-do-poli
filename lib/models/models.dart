1a1f07b78d9d792fce48b2c33ac0b76eff9f7350
brunopolii <brunopoli95@gmail.com>
2026-09-03 09:30:12 -0300

    feat: support workout weekdays and custom exercise data

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,4 +1,3 @@
-
 class Exercise {
   String id;
   String name;
@@ -8,206 +7,55 @@ class Exercise {
   List<double> weights;
   List<bool> done;
 
-  Exercise({
-    required this.id,
-    required this.name,
-    required this.muscle,
-    required this.sets,
-    required this.reps,
-    List<double>? weights,
-    List<bool>? done,
-  })  : weights = _fitDoubles(weights ?? const [], sets),
-        done = _fitBools(done ?? const [], sets);
+  Exercise({required this.id, required this.name, required this.muscle, required this.sets, required this.reps, List<double>? weights, List<bool>? done})
+      : weights = _fitDoubles(weights ?? const [], sets), done = _fitBools(done ?? const [], sets);
 
-  static List<double> _fitDoubles(List<double> values, int length) {
-    return List<double>.generate(
-      length,
-      (i) => i < values.length ? values[i] : 0,
-    );
-  }
+  static List<double> _fitDoubles(List<double> v, int n) => List<double>.generate(n, (i) => i < v.length ? v[i] : 0);
+  static List<bool> _fitBools(List<bool> v, int n) => List<bool>.generate(n, (i) => i < v.length ? v[i] : false);
 
-  static List<bool> _fitBools(List<bool> values, int length) {
-    return List<bool>.generate(
-      length,
-      (i) => i < values.length ? values[i] : false,
-    );
-  }
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'muscle': muscle,
-        'sets': sets,
-        'reps': reps,
-        'weights': weights,
-        'done': done,
-      };
-
-  factory Exercise.fromJson(Map<String, dynamic> json) {
-    final rawWeights = (json['weights'] as List?) ?? const [];
-    final rawDone = (json['done'] as List?) ?? const [];
-    return Exercise(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      muscle: (json['muscle'] as String?) ?? '',
-      sets: (json['sets'] as num?)?.toInt() ?? 3,
-      reps: (json['reps'] as num?)?.toInt() ?? 10,
-      weights: rawWeights.map((e) => (e as num).toDouble()).toList(),
-      done: rawDone.map((e) => e == true).toList(),
-    );
-  }
+  Map<String, dynamic> toJson() => {'id': id, 'name': name, 'muscle': muscle, 'sets': sets, 'reps': reps, 'weights': weights, 'done': done};
+  factory Exercise.fromJson(Map<String, dynamic> j) => Exercise(
+    id: j['id'] as String, name: j['name'] as String, muscle: (j['muscle'] as String?) ?? '',
+    sets: (j['sets'] as num?)?.toInt() ?? 3, reps: (j['reps'] as num?)?.toInt() ?? 10,
+    weights: ((j['weights'] as List?) ?? const []).map((e) => (e as num).toDouble()).toList(),
+    done: ((j['done'] as List?) ?? const []).map((e) => e == true).toList(),
+  );
 }
 
 class Workout {
   String id;
   String name;
   String date;
+  int weekday;
   List<Exercise> exercises;
-
-  Workout({
-    required this.id,
-    required this.name,
-    required this.date,
-    required this.exercises,
-  });
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'date': date,
-        'exercises': exercises.map((e) => e.toJson()).toList(),
-      };
-
-  factory Workout.fromJson(Map<String, dynamic> json) {
-    final rawExercises = (json['exercises'] as List?) ?? const [];
-    return Workout(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      date: (json['date'] as String?) ?? '',
-      exercises: rawExercises
-          .map((e) => Exercise.fromJson(Map<String, dynamic>.from(e as Map)))
-          .toList(),
-    );
+  Workout({required this.id, required this.name, required this.date, this.weekday = 1, required this.exercises});
+  Map<String, dynamic> toJson() => {'id': id, 'name': name, 'date': date, 'weekday': weekday, 'exercises': exercises.map((e) => e.toJson()).toList()};
+  factory Workout.fromJson(Map<String, dynamic> j) {
+    final date = (j['date'] as String?) ?? '';
+    int day = (j['weekday'] as num?)?.toInt() ?? 0;
+    if (day < 1 || day > 7) { try { day = DateTime.parse(date).weekday; } catch (_) { day = 1; } }
+    return Workout(id: j['id'] as String, name: j['name'] as String, date: date, weekday: day,
+      exercises: ((j['exercises'] as List?) ?? const []).map((e) => Exercise.fromJson(Map<String, dynamic>.from(e as Map))).toList());
   }
 }
 
 class Meal {
-  String id;
-  String date;
-  String type;
-  String food;
-  double calories;
-  double protein;
-  double carbs;
-  double fat;
-  String source;
-
-  Meal({
-    required this.id,
-    required this.date,
-    required this.type,
-    required this.food,
-    required this.calories,
-    required this.protein,
-    required this.carbs,
-    required this.fat,
-    this.source = 'manual',
-  });
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'date': date,
-        'type': type,
-        'food': food,
-        'calories': calories,
-        'protein': protein,
-        'carbs': carbs,
-        'fat': fat,
-        'source': source,
-      };
-
-  factory Meal.fromJson(Map<String, dynamic> json) => Meal(
-        id: json['id'] as String,
-        date: json['date'] as String,
-        type: (json['type'] as String?) ?? 'Refeição',
-        food: json['food'] as String,
-        calories: (json['calories'] as num).toDouble(),
-        protein: (json['protein'] as num).toDouble(),
-        carbs: (json['carbs'] as num).toDouble(),
-        fat: (json['fat'] as num).toDouble(),
-        source: (json['source'] as String?) ?? 'manual',
-      );
+  String id; String date; String type; String food; double calories; double protein; double carbs; double fat; String source;
+  Meal({required this.id, required this.date, required this.type, required this.food, required this.calories, required this.protein, required this.carbs, required this.fat, this.source = 'manual'});
+  Map<String, dynamic> toJson() => {'id': id, 'date': date, 'type': type, 'food': food, 'calories': calories, 'protein': protein, 'carbs': carbs, 'fat': fat, 'source': source};
+  factory Meal.fromJson(Map<String, dynamic> j) => Meal(id: j['id'] as String, date: j['date'] as String, type: (j['type'] as String?) ?? 'Refeição', food: j['food'] as String, calories: (j['calories'] as num).toDouble(), protein: (j['protein'] as num).toDouble(), carbs: (j['carbs'] as num).toDouble(), fat: (j['fat'] as num).toDouble(), source: (j['source'] as String?) ?? 'manual');
 }
 
 class MoneyTransaction {
-  String id;
-  String date;
-  String description;
-  String category;
-  double amount;
-  bool income;
-
-  MoneyTransaction({
-    required this.id,
-    required this.date,
-    required this.description,
-    required this.category,
-    required this.amount,
-    required this.income,
-  });
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'date': date,
-        'description': description,
-        'category': category,
-        'amount': amount,
-        'income': income,
-      };
-
-  factory MoneyTransaction.fromJson(Map<String, dynamic> json) =>
-      MoneyTransaction(
-        id: json['id'] as String,
-        date: json['date'] as String,
-        description: json['description'] as String,
-        category: (json['category'] as String?) ?? 'Outros',
-        amount: (json['amount'] as num).toDouble(),
-        income: json['income'] == true,
-      );
+  String id; String date; String description; String category; double amount; bool income;
+  MoneyTransaction({required this.id, required this.date, required this.description, required this.category, required this.amount, required this.income});
+  Map<String, dynamic> toJson() => {'id': id, 'date': date, 'description': description, 'category': category, 'amount': amount, 'income': income};
+  factory MoneyTransaction.fromJson(Map<String, dynamic> j) => MoneyTransaction(id: j['id'] as String, date: j['date'] as String, description: j['description'] as String, category: (j['category'] as String?) ?? 'Outros', amount: (j['amount'] as num).toDouble(), income: j['income'] == true);
 }
 
 class AgendaEvent {
-  String id;
-  String date;
-  String title;
-  String description;
-  String start;
-  String end;
-
-  AgendaEvent({
-    required this.id,
-    required this.date,
-    required this.title,
-    required this.description,
-    required this.start,
-    required this.end,
-  });
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'date': date,
-        'title': title,
-        'description': description,
-        'start': start,
-        'end': end,
-      };
-
-  factory AgendaEvent.fromJson(Map<String, dynamic> json) => AgendaEvent(
-        id: json['id'] as String,
-        date: json['date'] as String,
-        title: json['title'] as String,
-        description: (json['description'] as String?) ?? '',
-        start: (json['start'] as String?) ?? '08:00',
-        end: (json['end'] as String?) ?? '09:00',
-      );
+  String id; String date; String title; String description; String start; String end;
+  AgendaEvent({required this.id, required this.date, required this.title, required this.description, required this.start, required this.end});
+  Map<String, dynamic> toJson() => {'id': id, 'date': date, 'title': title, 'description': description, 'start': start, 'end': end};
+  factory AgendaEvent.fromJson(Map<String, dynamic> j) => AgendaEvent(id: j['id'] as String, date: j['date'] as String, title: j['title'] as String, description: (j['description'] as String?) ?? '', start: (j['start'] as String?) ?? '08:00', end: (j['end'] as String?) ?? '09:00');
 }
